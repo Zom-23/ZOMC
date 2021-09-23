616ecb4c1ad8d8e118cfd94996af58cfbd260391
Zom-23 <90399747+Zom-23@users.noreply.github.com>
2021-09-23 09:00:54 -0500

    Add files via upload

---
+++ DoubleVision.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib.Cards;
+using UnityEngine;
+using UnboundLib;
+using ZomC_Cards.MonoBehaviours;
+
+namespace ZomC_Cards.Cards
+{
+    class DoubleVision : CustomCard
+    {
+        DoubleVisionMono doubleVision;
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            doubleVision = player.gameObject.AddComponent<DoubleVisionMono>();
+        }
+
+        public override void OnRemoveCard()
+        {
+            Destroy(this.doubleVision);
+        }
+
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
+        {
+            cardInfo.allowMultiple = false;
+            gun.reloadTimeAdd = .25f;
+            gun.spread = 1.5f;
+        }
+
+     
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override string GetDescription()
+        {
+            return "You got drunk";
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Projectiles",
+                    amount = "Double",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLotOf
+                },
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Spread",
+                    amount = "150%",
+                    simepleAmount = CardInfoStat.SimpleAmount.aHugeAmountOf
+                },
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Reload",
+                    amount = "+.25s",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLittleBitOf
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.MagicPink;
+        }
+
+        protected override string GetTitle()
+        {
+            return "Double Vision";
+        }
+    }
+}
---
+++ GymCard.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib.Cards;
+using UnityEngine;
+using UnboundLib;
+
+namespace ZomC_Cards.Cards
+{
+    public class GymCard : CustomCard
+    {
+        public GymCard()
+        {
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {}
+
+        public override void OnRemoveCard()
+        {}
+
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
+        {
+            gun.reloadTimeAdd = .5f;
+            gun.damage = 1.5f;
+            statModifiers.sizeMultiplier = 1.5f;
+            statModifiers.health = 1.25f;
+            statModifiers.movementSpeed = .7f;
+        }
+
+        protected override UnityEngine.GameObject GetCardArt()
+        { return null; }
+
+        protected override string GetDescription()
+        {
+            return "Do you even lift bro?";
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Uncommon;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat
+                {
+                    positive = true,
+                    stat = "Damage",
+                    amount = "+50%",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLotOf
+                },
+                new CardInfoStat
+                {
+                    positive = true,
+                    stat = "Size",
+                    amount = "+50%",
+                    simepleAmount = CardInfoStat.SimpleAmount.Some
+                },
+                new CardInfoStat
+                {
+                    positive = true,
+                    stat = "Health",
+                    amount = "+50%",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLittleBitOf
+                },
+                new CardInfoStat
+                {
+                    positive = false,
+                    stat = "Speed",
+                    amount = "-30%",
+                    simepleAmount = CardInfoStat.SimpleAmount.slightlyLower
+                },
+                new CardInfoStat
+                {
+                    positive = false,
+                    stat = "Reload Speed",
+                    amount = "+.5s",
+                    simepleAmount = CardInfoStat.SimpleAmount.slightlyLower
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.FirepowerYellow;
+        }
+
+        protected override string GetTitle()
+        {
+            return "Gym";
+        }
+
+        public override string GetModName()
+        {
+            return "ZOMC";
+        }
+    }
+}
