123413e187e2d4b8d46676239ce6cd9e8a7d6797
Zom-23 <90399747+Zom-23@users.noreply.github.com>
2021-10-06 09:04:27 -0500

    Add files via upload

---
+++ Cards/FlappyBullets.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib.Cards;
+using UnityEngine;
+using UnboundLib;
+using ZomC_Cards.MonoBehaviours;
+
+
+namespace ZomC_Cards
+{
+    class FlappyBullets : CustomCard
+    {
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {  
+        }
+
+        public override void OnRemoveCard()
+        {  
+        }
+
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
+        {
+            gun.gravity = .5f;
+            gun.speedMOnBounce = 0f;
+            gun.waveMovement = true;
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override string GetDescription()
+        {
+            return "The bullets shall flap";
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Bounce",
+                    amount = "Lose all Speed",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Gravity",
+                    amount = "50%",
+                    simepleAmount = CardInfoStat.SimpleAmount.lower
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.NatureBrown;
+        }
+
+        protected override string GetTitle()
+        {
+            return "Flappy Bullets";
+        }
+
+        public override string GetModName()
+        {
+            return "ZOMC";
+        }
+    }
+}
