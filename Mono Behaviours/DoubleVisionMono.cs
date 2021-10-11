f4919453ca903c37658997ee64e63e7ee07a07dc
Zom-23 <90399747+Zom-23@users.noreply.github.com>
2021-10-11 08:59:08 -0500

    Add files via upload

--- Mono Behaviours/DoubleVisionMono.cs
+++ Mono Behaviours/DoubleVisionMono.cs
@@ -134,6 +134,8 @@ namespace ZomC_Cards.MonoBehaviours
             // set the gun of the spawnbulletseffect
             effect.SetGun(newGun);
             effect.SetGun(this.gun);
+            newGun.objectsToSpawn = new ObjectsToSpawn[] { PreventRecursion.stopRecursionObjectToSpawn };
+            this.gun.objectsToSpawn = new ObjectsToSpawn[] { PreventRecursion.stopRecursionObjectToSpawn };
         }
     }
     class DoubleViGun : Gun
