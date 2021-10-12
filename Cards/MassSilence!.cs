5a3c0023750252369d056f86749dc88121758261
Zom-23 <90399747+Zom-23@users.noreply.github.com>
2021-10-12 08:57:27 -0500

    Add files via upload

---
+++ Cards/MassSilence!.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib.Cards;
+using UnityEngine;
+using UnboundLib;
+using ZomC_Cards.MonoBehaviours;
+
+//Prevent others from firing for 3 seconds upon round start
+
+namespace ZomC_Cards.Cards
+{
+    public class MassSilence : CustomCard
+    {
+        public MassSilence()
+        {
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            
+        }
+
+        public override void OnRemoveCard()
+        { }
+
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
+        {
+            
+        }
+
+        protected override UnityEngine.GameObject GetCardArt()
+        { return null; }
+
+        protected override string GetDescription()
+        {
+            return "Everyone stop shooting";
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Rare;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat
+                {
+
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.MagicPink;
+        }
+
+        protected override string GetTitle()
+        {
+            return "Mass Silence";
+        }
+
+        public override string GetModName()
+        {
+            return "ZOMC";
+        }
+    }
+}
---
+++ Cards/Turret.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib.Cards;
+using UnityEngine;
+using UnboundLib;
+using ZomC_Cards.MonoBehaviours;
+using Photon.Pun;
+
+namespace ZomC_Cards
+{
+    class Turret : CustomCard
+    {
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            block.cdAdd = 1f;
+            block.objectsToSpawn.Add(PhotonNetwork.Instantiate("Player", player.transform.position, Quaternion.identity));
+        }
+
+        public override void OnRemoveCard()
+        {
+        }
+
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
+        {
+            
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override string GetDescription()
+        {
+            return "Block to spawn a turret";
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Rare;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return null;
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.TechWhite;
+        }
+
+        protected override string GetTitle()
+        {
+            return "Turret";
+        }
+    }
+}
