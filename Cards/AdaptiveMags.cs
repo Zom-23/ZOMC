4a875a288c4cce927479681bba0bc9d18c494aeb
Zom-23 <90399747+Zom-23@users.noreply.github.com>
2021-11-02 09:27:00 -0500

    Add files via upload

---
+++ Cards/AdaptiveMags.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib.Cards;
+using UnityEngine;
+using UnboundLib;
+
+//Increases the maximum ammo after emptying your magazine, resets between rounds
+
+namespace ZomC_Cards.Cards
+{
+    class AdaptiveMags : CustomCard
+    {
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            gunAmmo.maxAmmo = 3;
+        }
+
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
+        {
+            gun.reloadTimeAdd = (float)-(gun.reloadTime * .5);
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override string GetDescription()
+        {
+            return "Your magazine grows";
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Uncommon;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat
+                {
+                    positive = true,
+                    stat = "Reload",
+                    amount = "-50%",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLotLower
+                },
+                new CardInfoStat
+                {
+                    positive = true,
+                    stat = "ammo",
+                    amount = "3",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.DefensiveBlue;
+        }
+
+        protected override string GetTitle()
+        {
+            return "Adaptive Magazines";
+        }
+    }
+}
