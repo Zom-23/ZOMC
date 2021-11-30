e4b633b7c17d267414835818d025e3150a0349a8
Zom-23 <90399747+Zom-23@users.noreply.github.com>
2021-11-30 09:27:30 -0600

    Add files via upload

---
+++ Cards/Sloth.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib.Cards;
+using UnityEngine;
+using UnboundLib;
+using UnboundLib.GameModes;
+using System.Collections;
+
+namespace ZomC_Cards.Cards
+{
+    class Sloth : CustomCard
+    {
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            Player[] players = PlayerManager.instance.players.ToArray();
+            foreach(Player p in players)
+            {
+                p.data.stats.gravity *= 1.3f;
+                p.data.weaponHandler.gun.gravity *= 1.2f;
+                p.data.stats.slow = .5f;
+            }
+        }
+
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            statModifiers.movementSpeed = .8f;
+            statModifiers.lifeSteal = 1.5f;
+            statModifiers.secondsToTakeDamageOver = 3f;
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override string GetDescription()
+        {
+            return "Everyone should just slow down";
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Rare;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat
+                {
+                    positive = false,
+                    stat = "Speed",
+                    amount = "-20%",
+                    simepleAmount = CardInfoStat.SimpleAmount.slightlyLower
+                },
+                new CardInfoStat
+                {
+                    positive = true,
+                    stat = "Life Steal",
+                    amount = "+50%",
+                    simepleAmount = CardInfoStat.SimpleAmount.Some
+                },
+                new CardInfoStat
+                {
+                    positive = false,
+                    stat = "Take Damage Over Time",
+                    amount = "2s",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                new CardInfoStat
+                {
+                    positive = true,
+                    stat = "Everyone",
+                    amount = "Heavier",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.EvilPurple;
+        }
+
+        protected override string GetTitle()
+        {
+            return "Sin: Sloth";
+        }
+
+        public override string GetModName()
+        {
+            return "ZOMC";
+        }
+    }
+}
