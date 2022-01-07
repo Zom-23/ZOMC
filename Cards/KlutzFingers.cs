3019cf76bf464418e826318972453294da598a27
Zom-23 <90399747+Zom-23@users.noreply.github.com>
2022-01-07 08:01:44 -0600

    Add files via upload

---
+++ Cards/KlutzFingers.cs
@@ -0,0 +1,79 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using WWC.Extensions;
+using WWC.MonoBehaviours;
+using WillsWackyManagers.Utils;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using UnityEngine;
+
+namespace WWC.Cards.Curses
+{
+    class KlutzFingers : CustomCard
+    {
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            characterStats.OnReloadDoneAction += reloadFail;
+            
+            void reloadFail(int i)
+            {
+                System.Random random = new System.Random();
+                int roll = random.Next(100);
+                if(roll <= 20)
+                {
+                    gunAmmo.SetFieldValue("currentAmmo", 0);
+                }
+            }
+        }
+
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            cardInfo.allowMultiple = false;
+            cardInfo.categories = new CardCategory[] { CurseManager.instance.curseCategory };
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override string GetDescription()
+        {
+            return "Finger coordination issues";
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Uncommon;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat
+                {
+                    positive = false,
+                    stat = "Reload Fail Chance",
+                    amount = "20%",
+                    simepleAmount = CardInfoStat.SimpleAmount.Some
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.TechWhite;
+        }
+
+        protected override string GetTitle()
+        {
+            return "Klutz Fingers";
+        }
+    }
+}
+*/
